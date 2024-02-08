78ee103f584ef68a2b85221120c2e0ed1f8d3ec4
bucketmedia <61097620+bucketmedia@users.noreply.github.com>
2024-02-08 15:36:58 +0000

    create component queryinputfield

---
+++ src/app/queryinputfield/queryinputfield.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'query-input-field',
+  standalone: true,
+  imports: [],
+  styleUrl: './queryinputfield.component.scss',
+  templateUrl: './queryinputfield.component.html',
+})
+export class QueryInputFieldComponent {
+  @Input({ required: true }) queryKey!: string;
+  @Input({ required: true }) label!: string;
+  @Input() fieldType = 'text';
+
+  inputVal = '';
+
+  constructor(public router: Router, public route: ActivatedRoute) {}
+
+  onChange(e: any) {
+    this.router.navigate([''], {
+      queryParams: { [this.queryKey]: e.target.value },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((p) => {
+      this.inputVal = p.get(this.queryKey) || '';
+    });
+  }
+}
